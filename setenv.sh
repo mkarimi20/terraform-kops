504593199c2740d909248ba0814400b8015f64a7
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-11 12:58:47 -0600

    terraform version changed

--- setenv.sh
+++ setenv.sh
@@ -77,4 +77,8 @@ region = "${S3BUCKETREGION}"
 EOF
 cat backend.tf
 rm -rf .terraform/terraform.tfstate
-terraform init
+terraform_0.11.14 init
+
+echo "################"
+echo "Please use terraform_0.11.14"
+echo "################"ß
