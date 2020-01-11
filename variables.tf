fb790cb0f6d2ffb58ae61369fb290238b6e01902
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-11 14:34:08 -0600

    vpc variable changed

--- variables.tf
+++ variables.tf
@@ -2,5 +2,7 @@ variable "max_size" {}
 variable "min_size" {}
 variable "bastion_max-size" {}
 variable "bastion_min-size" {}
+variable "cidr_block" {}
+
 
 
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "ops-work-net" {
-  cidr_block           = "172.20.0.0/16"
+  cidr_block           = "${var.cidr_block}"                           #"172.20.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
